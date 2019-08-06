72b2ee3dc2ed932f7ac2da96c2322151d6cff2d1
robmarr <rob@marr.london>
2019-08-06 08:42:06 +0100

    fix tests for upgraded mochify

--- test/request.js
+++ test/request.js
@@ -5,8 +5,8 @@ var assert = require('assert');
 describe('Request', function () {
 	it('should have the correct protocol', function () {
 		var r = new Request();
-		assert.equal(r.protocol, 'http');
-		assert.equal(r.secure, false);
+		assert.equal(r.protocol, 'https');
+		assert.equal(r.secure, true);
 	});
 	it('should have the correct hostname', function () {
 		var r = new Request();
